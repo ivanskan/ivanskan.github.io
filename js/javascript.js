7722ee7d93f4b18839c2ca53cb38fd888a0d12b9
ivan <ivan@Pk>
2017-06-11 13:42:51 -0500

    script carrusel

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,19 @@
-/**
- * java script here :D 
- */
+
+/* java script here :D */
+
+  //carrusel
+  var slideIndex = 1;
+  showDivs(slideIndex);
+  function plusDivs(n) {
+  showDivs(slideIndex += n);
+  }
+  function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+  }
