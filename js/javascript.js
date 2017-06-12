bff045f3047fb16173297fa36aeebd2f10c9cca6
ivan <ivan@Pk>
2017-06-11 19:25:49 -0500

    script buscar + tabs

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,34 @@
 
-/* java script here :D */
 
+
+  function msg(){
+    alert("Mesanje enviado gracias por colaborar con la pagina, tendremos en cuenta su opinion")
+  }
+
+  // Script to open and close sidebar
+  function w3_open() {
+      document.getElementById("mySidebar").style.display = "block";
+      document.getElementById("myOverlay").style.display = "block";
+  }
+  function w3_close() {
+      document.getElementById("mySidebar").style.display = "none";
+      document.getElementById("myOverlay").style.display = "none";
+  }
+
+  //tabs
+  function openLink(evt, animName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("city");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+       tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+    }
+    document.getElementById(animName).style.display = "block";
+    evt.currentTarget.className += " w3-red";
+  }
   //carrusel
   var slideIndex = 1;
   showDivs(slideIndex);
@@ -17,13 +45,20 @@
     }
     x[slideIndex-1].style.display = "block";
   }
-  
-  // Script to open and close sidebar
-  function w3_open() {
-      document.getElementById("mySidebar").style.display = "block";
-      document.getElementById("myOverlay").style.display = "block";
-  }
-  function w3_close() {
-      document.getElementById("mySidebar").style.display = "none";
-      document.getElementById("myOverlay").style.display = "none";
-  }
+
+
+//buscar
+function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("mySidebar");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
